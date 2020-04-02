c6f5060e815452f321fc6f654182865c5ffb1b35
Gulyuki Aleksey <gulyuki_a@bk.ru>
2020-04-02 12:29:26 +0300

    add Main.java

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int bank = 2_000_000_000;
+        int payment = 500_000_000;
+        int total = bank + payment;
+        System.out.println(total);
+    }
+}
